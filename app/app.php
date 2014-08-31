c788bc9037515393191113337fd424f824402f75
Fabian Frick <fabi.kcirf@gmail.com>
2014-08-31 19:52:52 +0200

    created base layout

--- app/app.php
+++ app/app.php
@@ -3,10 +3,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+
 $app['debug'] = true;
+$app['root_dir'] = __DIR__.'/../';
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => $app['root_dir'].'views',
+));
+
 
 $app->get('/', function() use ($app) {
-    return $app['twig']->render('index.html.twig');
+    return $app['twig']->render('index.twig');
 });
 
 return $app;
