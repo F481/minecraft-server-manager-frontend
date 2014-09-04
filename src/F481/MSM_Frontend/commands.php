b88937d4118be20f135bbc0bfd873cbda2b662d8
Fabian Frick <fabi.kcirf@gmail.com>
2014-09-04 14:50:57 +0200

    added setup and management commands

---
+++ src/F481/MSM_Frontend/commands.php
@@ -0,0 +1,28 @@
+<?php
+
+# Setup Commands
+define('server_list', 'server list');
+define('server_create', 'server create $name');
+define('server_delete', 'server delete $name');
+define('server_rename', 'server rename $name $new-name');
+
+# Server Mangement Commands
+define('start', '$server start');
+define('stop', '$server stop');
+define('restart', '$server restart');
+define('status', '$server status');
+define('connected', '$server connected');
+define('worlds_list', '$server worlds list');
+define('worlds_load', '$server worlds load');
+define('worlds_ram', '$server worlds ram $world');
+define('worlds_todisk', '$server worlds todisk');
+define('worlds_backup', '$server worlds backup');
+define('worlds_on', '$server worlds on $world');
+define('worlds_off', '$server worlds off $world');
+define('logroll', '$server logroll');
+define('backup', '$server backup');
+define('jar', '$server jar $jargroup $file');
+define('console', '$server console');
+define('config', '$server config $setting $value');
+
+
